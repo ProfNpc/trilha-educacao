b9bf91cdec6a610ba134b786fe6e22c019ad0790
Alessandro <professornpc@gmail.com>
2022-10-20 20:17:12 -0300

    Projeto Base

---
+++ src/main/java/com/belval/trilhaeducacao/TrilhaeducacaoApplication.java
@@ -0,0 +1,13 @@
+package com.belval.trilhaeducacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilhaeducacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilhaeducacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belval/trilhaeducacao/TrilhaeducacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belval.trilhaeducacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrilhaeducacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
