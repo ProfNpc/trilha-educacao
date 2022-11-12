9f40f5c5de1ca518bbfe5b05bdc06af153ebcb41
Kaique <kaiqueoliveira375@gmail.com>
2022-11-11 21:17:49 -0300

    Correção SpringApplication.run()

--- src/main/java/com/trilhaeducacao/app1/TrilhaAplication.java
+++ src/main/java/com/trilhaeducacao/app1/TrilhaAplication.java
@@ -1,5 +1,6 @@
 package com.trilhaeducacao.app1;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @SpringBootApplication
 public class TrilhaAplication {
-
 	public static void main(String[] args) {
-
+		SpringApplication.run(TrilhaAplication.class, args);
 	}
-
-	@GetMapping("/cadastro")
-
-	public ModelAndView getList() {
-
-		ModelAndView mv = new ModelAndView("cadastro");
-		return mv;
-	}
-
 }
