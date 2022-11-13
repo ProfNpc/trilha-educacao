72375f85bf53ce9125b506026c788444e531d9a5
Kaique <kaiqueoliveira375@gmail.com>
2022-11-13 13:40:37 -0300

    Atualizando banco de dados

--- src/main/java/com/trilhaeducacao/app1/model/Usuario.java
+++ src/main/java/com/trilhaeducacao/app1/model/Usuario.java
@@ -1,84 +1,50 @@
 package com.trilhaeducacao.app1.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
 
-	private int id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
 	private String nome;
 	private String sobrenome;
-	private String email;
 	private int celular;
-
-	
-	
-	public int getId() {
-		return id;
+	public Usuario() {
+		super();
 	}
-
-
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-
-
-	public String getNome() {
-		return nome;
-	}
-
-
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-
-	public String getSobreNome() {
-		return sobrenome;
-	}
-	
-	public void setSobreNome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-	
-
-	
-	
-	public String getEmail() {
-		return email;
-	}
-
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-
-	public int getCelular() {
-		return celular;
-	}
-
-
-
-	public void setCelular(int celular) {
-		this.celular = celular;
-	}
-
-
-
-	public Usuario(String nome, int id, String sobrenome, String email, int celular) {
+	public Usuario(Integer id, String nome, String sobrenome, int celular) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.sobrenome = sobrenome;
-		this.email = email;
 		this.celular = celular;
-		
-		
 	}
-	
-	
-}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobreNome() {
+		return sobrenome;
+	}
+	public void setDescricao(int celular) {
+		this.celular = celular;
+	}
 
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + ", celular=" + celular + "]";
+	}
+}
